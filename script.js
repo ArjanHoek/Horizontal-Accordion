76f2cffc211e947c107d5214701cdac1b3b47593
Arjan Hoek <arjanhoek5191@gmail.com>
2022-01-07 13:50:14 +0100

    create basic accordion with animation

---
+++ script.js
@@ -0,0 +1,30 @@
+const items = document.querySelectorAll('.item');
+
+let lock = false;
+
+const handleOpen = cur => {
+  if (lock) {
+    return;
+  }
+
+  lock = true;
+
+  setTimeout(() => {
+    lock = false;
+  }, 500);
+
+  items.forEach(item => {
+    if (item === cur) {
+      item.classList.add('open');
+    } else {
+      item.classList.remove('open');
+    }
+  });
+};
+
+items.forEach(item => {
+  const btn = item.querySelector('.open-btn');
+  btn.addEventListener('click', function () {
+    handleOpen(item);
+  });
+});
